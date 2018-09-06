edd51a23647e7f60a71ee9f69c9061cd3eb5747b
TusharIITR03 <tushariitr3@gmail.com>
2018-09-06 18:37:57 +0530

    COmpleted p1 and p2

--- n4/p1/Rwa1.py
+++ n4/p1/Rwa1.py
@@ -36,6 +36,7 @@ plt.rcParams['xtick.top'] = plt.rcParams['xtick.labeltop'] = True
 
 plt.plot(GR[:],Y[:])
 plt.axis([0, 200, 22750, 19900])
+plt.xlabel("GR")
 plt.show()
 
 ###################ESTIMATING Vsh################
@@ -49,20 +50,22 @@ plt.rcParams['xtick.top'] = plt.rcParams['xtick.labeltop'] = True
 
 plt.plot(NPOR[:],Y[:])
 plt.axis([3, 1, 22750, 19900])
+plt.xlabel("NPOR")
 plt.show()
 
 ##################POROSITY#######################
 PHI=(((Vsh*RHOsh)+(1-Vsh)*RHOmatrix)-RHOb)/(((Vsh*RHOsh)+(1-Vsh)*RHOmatrix)-1)
 
-####################PHI PLOT#####################
+
+####################Initial PHI PLOT#####################
 plt.rcParams['xtick.bottom'] = plt.rcParams['xtick.labelbottom'] = False
 plt.rcParams['xtick.top'] = plt.rcParams['xtick.labeltop'] = True
 
 plt.plot(PHI[:],Y[:])
 plt.axis([1, 2, 22750, 19900])
+plt.xlabel("Initial PHI")
 plt.show()
-
-####################Rwa PLOT####################
+####################Initial Rwa PLOT####################
 Rwa=np.multiply(Ro,np.power(PHI,2))
 
 plt.rcParams['xtick.bottom'] = plt.rcParams['xtick.labelbottom'] = False
@@ -70,22 +73,44 @@ plt.rcParams['xtick.top'] = plt.rcParams['xtick.labeltop'] = True
 
 plt.plot(Rwa[:],Y[:])
 plt.axis([0, 10, 22750, 19900])
+plt.xlabel("Rwa Initial")
 plt.show()
 
 ###################CALCULATING SALINITY#########
-PHIprev=PHI[4881]
+PHIprev=PHI
 for i in range(10):
-    Rwa=np.multiply(Ro[4881],np.power(PHIprev,2))
-    print("Rwa is :")
-    print(Rwa)
+    Rwa=np.multiply(Ro,np.power(PHIprev,2))
     S=np.power(10,(1/0.995*(np.log10(3647.5)-np.log10(Rwa*(T[4881]/81.77+6.77/81.77)-0.0123))))
     S=S/1000000
-    RHOfluid=RHOwater[4881]+S*(0.668+0.44*S+1/1000000*(300*P[4881]-2400*P[4881]*S+T[4881]*(80+3*T[4881]-3300*S-13*P[4881]+47*P[4881]*S)))
-    PHI=(((Vsh[4881]*RHOsh)+(1-Vsh[4881])*RHOmatrix)-RHOb[4881])/(((Vsh[4881]*RHOsh)+(1-Vsh[4881])*RHOmatrix)-RHOfluid)
-    print("PHI is : ")
-    print(PHI)
+    RHOfluid=RHOwater+S*(0.668+0.44*S+1/1000000*(300*P-2400*P*S+T*(80+3*T-3300*S-13*P+47*P*S)))
+    PHI=(((Vsh*RHOsh)+(1-Vsh)*RHOmatrix)-RHOb)/(((Vsh*RHOsh)+(1-Vsh)*RHOmatrix)-RHOfluid)
     PHIprev=PHI
 
+
 ####################Salinity PLOT####################
-print("Salinity is :")
-print(S)
+plt.rcParams['xtick.bottom'] = plt.rcParams['xtick.labelbottom'] = False
+plt.rcParams['xtick.top'] = plt.rcParams['xtick.labeltop'] = True
+
+plt.plot(S[:],Y[:])
+plt.axis([0, 0.003, 22750, 19900])
+plt.xlabel("Salinity")
+plt.show()
+
+
+####################Revised PHI PLOT#####################
+plt.rcParams['xtick.bottom'] = plt.rcParams['xtick.labelbottom'] = False
+plt.rcParams['xtick.top'] = plt.rcParams['xtick.labeltop'] = True
+
+plt.plot(PHI[:],Y[:])
+plt.axis([1, 2, 22750, 19900])
+plt.xlabel("Revised PHI")
+plt.show()
+
+####################Revised Rwa PLOT####################
+plt.rcParams['xtick.bottom'] = plt.rcParams['xtick.labelbottom'] = False
+plt.rcParams['xtick.top'] = plt.rcParams['xtick.labeltop'] = True
+
+plt.plot(Rwa[:],Y[:])
+plt.axis([0, 10, 22750, 19900])
+plt.xlabel("Rwa Revised")
+plt.show()
